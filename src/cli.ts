e40b0b2b4d5515a4e7de2dee503189ff86c46df5
Kathleen Garrity <kathleen.garrity223@gmail.com>
2025-06-06 15:28:36 -0400

    fix: better name for cli command

--- src/cli.ts
+++ src/cli.ts
@@ -13,7 +13,7 @@ const program = new Command();
 const { prompt } = enquirer;
 
 program
-  .name("create-vite-react-mui-project")
+  .name("start-vite-react-mui-project")
   .description("Bootstrap a custom Vite + React + TypeScript project with MUI")
   .action(async () => {
     const { name } = await prompt<{ name: string }>({
